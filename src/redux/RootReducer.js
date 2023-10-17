41547b0d463102a91118c8fd827bc3db20685f05
Sudhir Kumar <sudhir.kumar@credflow.in>
2023-10-17 09:19:35 +0530

    removed old docs

--- src/redux/RootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import sidebarReducer from "./SideBarOpener";
-
-const RootReducer = combineReducers({
-    rh_sidebar: sidebarReducer
-})
-
-export default RootReducer;
